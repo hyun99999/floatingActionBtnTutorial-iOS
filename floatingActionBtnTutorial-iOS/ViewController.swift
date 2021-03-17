50dc6dd2593b54427c62228d29b095c3741b3ef7
hyungyu Kim <ypsophila97@icloud.com>
2021-03-17 23:02:31 +0900

    implement floatingActionButton quickStart

--- floatingActionBtnTutorial-iOS/ViewController.swift
+++ floatingActionBtnTutorial-iOS/ViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import JJFloatingActionButton
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let actionButton = JJFloatingActionButton()
+
+        actionButton.addItem(title: "item 1", image: UIImage(named: "First")?.withRenderingMode(.alwaysTemplate)) { item in
+          // do something
+        }
+
+        actionButton.addItem(title: "item 2", image: UIImage(named: "Second")?.withRenderingMode(.alwaysTemplate)) { item in
+          // do something
+        }
+
+        actionButton.addItem(title: "item 3", image: nil) { item in
+          // do something
+        }
+
+        view.addSubview(actionButton)
+        //autolayout
+//        actionButton.translatesAutoresizingMaskIntoConstraints = false
+//        actionButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+//        actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16).isActive = true
+
+        // last 4 lines can be replaced with
+         actionButton.display(inViewController: self)
     }
 
 
